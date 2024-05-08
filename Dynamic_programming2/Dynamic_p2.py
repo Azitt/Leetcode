f6332214a5dc0f90cb4fe8e0fcb1cb448544f330
Azitt <az.tayyebi@gmail.com>
2024-05-08 10:49:55 -0400

    dynamic2

--- Dynamic_programming2/Dynamic_p2.py
+++ Dynamic_programming2/Dynamic_p2.py
@@ -1,4 +1,4 @@
- 
+##naive approach############################### 
 def word_break2(s,word_dict):
     def backtrack(prefix,suffix):
      if not suffix:
@@ -14,7 +14,7 @@ def word_break2(s,word_dict):
 s = "catsanddog"
 word_dict = ["cat", "cats", "and", "sand", "dog"]
 print(word_break2(s, word_dict))
-
+###dynamic programming approach#########################
 def word_break2(s, word_dict):
     dp = [[]] * (len(s) + 1)
     dp[0] = [""]
@@ -39,6 +39,45 @@ def word_break2(s, word_dict):
 s = "catsanddog"
 word_dict = ["cat", "cats", "and", "sand", "dog"]
 print(word_break2(s, word_dict))
+
+###Decode Ways ########################################### 
+def num_of_decodings(decode_str):
+  n = len(decode_str)
+  dp = [0]*(n+1)
+  dp[0]= 1
+  if decode_str[0] != 0:
+      dp[1] = 1
+  else:    
+      return 0
+  
+  for i in range(2,n+1):
+      if decode_str[i-1] != '0':
+          dp[i] += dp[i-1] 
+      if decode_str[i-2] =='1' or (decode_str[i-2] == '2' and decode_str[i-1]<='6'):
+          dp[i] += dp[i-2]    
+  return dp[n]                
+
+
+decode_str = "124"
+print(num_of_decodings(decode_str))
+
+### Climbing Stairs##########################################
+def climb_stairs(n):   
+    dp = [0]*(n+1)
+    dp[0],dp[1] = 1,1
+    for i in range(2,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]    
+print(climb_stairs(4))
+
+
+
+
+
+
+
+
+
 
 
                  
--- Dynamic_programming2/README.md
+++ Dynamic_programming2/README.md
@@ -1,2 +1,11 @@
 # word break 2 #######################
 ![alt text](image.png)
+
+# Decode way###########################
+![alt text](image-1.png)
+![alt text](image-2.png)
+
+# Climb Strairs############################
+You are climbing a staircase. It takes n steps to reach the top. Each time, you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+![alt text](image-3.png)
+
